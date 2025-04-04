6fea1fbfec8579bb085b5753219f8becb269b2a7
ankita chowdhury <ankitachowdhury145@gmail.com>
2025-04-04 19:56:21 +0530

    update

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import ReactDom from 'react-dom/client';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App' ;
+
+import './index.css';
+
+import { PrivyProvider } from '@privy-io/react-auth';
+
+const root = ReactDom.createRoot(document.getElementById("root")) ;
+
+root.render(
+
+    <PrivyProvider
+    appId="your-privy-app-id"
+    clientId="cm8m00l9j00chee25x6xeqoxq"
+    config={{
+      // Display email and wallet as login methods
+      appearance: {
+        theme: 'dark',
+        
+      },
+     
+    }}
+  >
+  <Router>
+    <App />
+  </Router>
+    <App />
+  </PrivyProvider>
+
+);
