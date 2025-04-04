f1abf5810df3882e68919076dae417ff2ad6e2d2
0888888888 <anshuraj9905151156@gmail.com>
2025-04-04 20:48:51 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar" ;
 
 const App = () => {
   return (
